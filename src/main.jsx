4d1b2aaf51aaccc86ea546bfa07211a4fe8918e3
zahadneokurkycz <filip.belzik@email.cz>
2024-05-14 19:46:44 +0200

    Installed vercel's speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { UI, Theme } from './UI.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UI />
 	<Theme />
+	<SpeedInsights/>
   </React.StrictMode>,
 )
