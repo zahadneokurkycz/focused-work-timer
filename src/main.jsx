f5b59a9ff5d0cb0b41ebdacf1df1e38275b70ddf
zahadneokurkycz <filip.belzik@email.cz>
2024-05-14 19:55:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { UI, Theme } from './UI.jsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
